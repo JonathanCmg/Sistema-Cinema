bfb1e4345daafb93323446b9ca46aee6fd29c4c5
Jonathan <Jonat@DESKTOP-LTORHGB>
2025-07-02 16:01:23 -0300

    Atualização do arquivo de texto

---
+++ src/main/java/br/edu/ifpr/bsi/cinema/projeto/cinema/Leia-Me.ts
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/TypeScriptDataObjectTemplate.ts to edit this template
+ */
+Temos que criar todas essas funcionalidades sintam-se livres para adicionar
+ou remover quaisquer itens listados, lembrem-se de sempre atualizar a tabela
+e sempra dar pull(Atualizar o seu projeto) e push (enviar o seu projeto)
+
+Criar Classes 
+Salas
+Filmes
+Ingresso
+
+Criar funcionalidades
+Capacidade das salas
+Criar atributos dos filmes
+Registro de novos filmes
+Criar funcionamento das sessões
+Criar compra de ingressos
+Atributos dos ingressos
